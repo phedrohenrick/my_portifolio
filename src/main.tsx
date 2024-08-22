0effe20d76bceebc6cd70c65e89c5ce29e8b4127
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-08-22 19:20:40 -0300

    style: Styles the hero screen components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  
+     //tudo dentro do app consegue pegar a cor do thema
+ <StrictMode>
+  
+    <ThemeProvider theme={theme}> 
+    <CssBaseline/>
     <App />
+    </ThemeProvider>
+  
   </StrictMode>,
 )
