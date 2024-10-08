836e950646e3232159d4a2a17841ecebed7d3428
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-10-08 20:46:23 -0300

    refactor: remove unnecessary codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home/Home"
-
+import Navbar from './components/NavBar/NavBar';
+import './App.css';
 
 const App = () =>{  //uma notação diferente para function 
 
   return (
     <>
+    <Navbar/>
     <Home/>
     </>
   )
