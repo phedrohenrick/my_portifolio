828020aa44cbd369fbaf829f19d15d6a63f5834f
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-10-14 16:54:21 -0300

    Style: settle the about section (Skills, self informatios)

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -1,15 +1,24 @@
 import styled from "@emotion/styled"
-import theme from "../../../../theme";
+import Git from "../../../../assets/images/imageSkills/Git.png"
+import Docker from "../../../../assets/images/imageSkills/Docker.png"
+import Angular from "../../../../assets/images/imageSkills/Angular.png"
+import Spring from "../../../../assets/images/imageSkills/Spring.png"
+import Java from "../../../../assets/images/imageSkills/Java.png"
+import React from "../../../../assets/images/imageSkills/React.png"
+import fish from "../../../../assets/images/ImageBackground/fish.gif"
+import typescript from "../../../../assets/images/imageSkills/Typescript.png"
+import c from "../../../../assets/images/imageSkills/C.png"
+import postgesql from "../../../../assets/images/imageSkills/PostgreSql.png"
+
 import { Box, Container, Grid, Typography } from "@mui/material";
 import AnimatedBackground from "../../../../components/AnimatedBackground/AboutBackground.gif";
+import AnimatedBackgroundteste from "../../../../components/AnimatedBackground/ProjectScreen.gif";
+import StyledCard from "../../../../components/StyledCard/StyledCard"
 
 
+const About = () =>{ //uma notação diferente para function 
 
-
-const About = () =>{  //uma notação diferente para function 
-
-
-    const StyledAbout = styled("div")(({theme}) => ({
+    const StyledAbout = styled("div")(({}) => ({
       backgroundImage: `url(${AnimatedBackground})`,
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
@@ -25,34 +34,109 @@ const About = () =>{  //uma notação diferente para function
     },
 
     '@media (max-width: 480px)': {
-      height: '70vh',  // Altura menor para telas menores
-    }
+      height: 'auto',  // Altura menor para telas menores
+    },
+    
+    
       }))
 
       const StyledImg = styled("img")(() => ({
-        width:"90%",
+        width:"135%",
         borderRadius :"50%",
+        margin: '-100px',
   }))
+  
+  const StyledImgfish = styled("img")(() => ({
+
+    display: "none",
+
+   '@media (max-width: 768px)': { // Para telas com largura menor que 768px
+        display: "block", // Exibir em dispositivos móveis
+        width: "100%", // Ajuste a largura para ser responsiva (100% da área disponível)
+        maxWidth: "130%", // Limite o crescimento máximo
+        borderRadius: "50%", // Aplicando bordas arredondadas
+        margin:"-px",
+
+    }
+    
+
+  }))
+    
     
     return (
       <>
 
         <StyledAbout id="sobre">
           <Container>
+            <Grid container spacing={2} alignItems="center" gap={"40px"} >
 
-      
-            <Grid container spacing={2} alignItems="center">
-              <Grid item xs={12} md = {5}>
-              <Typography variant="h5" color="primary" textAlign="left" pb={2} fontWeight={"bold"}>
-                Olá! Sou o <Box component="span" color={"#E67E22"}>Phedro</Box>, estudante de Ciência da Computação.
-                Como desenvolvedor web e full stack, estou construindo uma experiência sólida na construção de APIs utilizando o <Box component="span" color={'#00bc58'}>Spring Framework</Box>, com integração a bancos de dados usando o <Box component="span" color={'#00bc58'}>PostgreSQL</Box>.
-              </Typography>
-
+              <Grid item xs= {10}>
+              <StyledImgfish src= {fish}/>
               </Grid>
 
-              <Grid item xs={12} md = {6} >
-              <Typography variant="h5" color = "primary" textAlign="left"  pb={2} fontWeight= {"bold"} >No back-end, foco em entregar soluções eficientes e escaláveis, 
-              utilizando as melhores práticas de desenvolvimento Java com spring. No front-end, concentro meus estudos em <Box component="span" color={'#00bc58'}>  Angualar e React</Box> para garantir interfaces elegantes. </Typography>
+            <Grid item xs={12} md = {6} marginTop={"-40px"} >
+              <Typography variant="h2" color = "primnary">
+                  Sobre mim
+              </Typography>
+            </Grid>
+      
+              <Grid item xs={12} md = {5}>
+                <Typography variant="h5" color="primary" textAlign="left" pb={2} fontWeight={"bold"} marginTop={"-40px"}>
+                Olá! Sou o <Box component="span" color={"#E67E22"}>Phedro</Box>, estudante de Ciência da Computação.
+                  Como desenvolvedor web e full stack, estou construindo uma experiência sólida na construção de APIs, com integração a bancos de dados.
+              </Typography>
+              </Grid>
+
+              <Grid item xs={12} md = {6}>
+                <Typography variant="h5" color = "primary" textAlign="left"  pb={2} fontWeight= {"bold"} marginTop={"-40px"} >No back-end, foco em entregar soluções eficientes e escaláveis, 
+                        utilizando as melhores práticas de desenvolvimento Java com spring. No front-end, concentro meus estudos em <Box component="span" color={'#E67E22'}>  Angualar e React</Box> para garantir interfaces elegantes. </Typography>
+              </Grid>
+              
+              <Grid item xs={12} md = {4} >
+                      <Typography variant="h2" color = "primnary">
+                        Skills
+                      </Typography>
+                </Grid>
+                <Grid container justifyContent="center">
+                  <Grid item xs={12} md={1} >
+                    <StyledCard >
+                      <Grid container justifyContent="center" spacing={1} alignItems="center">
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={Git} alt="Git" />
+                        </Grid>
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={Java} alt="Java" />
+                        </Grid>
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={Angular} alt="Angular" />
+                        </Grid>
+
+
+                       
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={Spring} alt="Spring" />
+                        </Grid>
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={React} alt="React" />
+                        </Grid>
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={Docker} alt="Docker" />
+                        </Grid>
+
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={c} alt="c" />
+                        </Grid>
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={postgesql} alt="PostgreSql" />
+                        </Grid>
+                        <Grid item xs={4} md={4}>
+                          <StyledImg src={typescript} alt="typescript" />
+                        </Grid>
+
+
+                      </Grid>
+                      </StyledCard>
+                  </Grid>
               </Grid>
               
             </Grid>
