f21ded344f1cfd345eae5410d61d9dc5868ecd41
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-10-25 16:28:27 -0300

    Config: adding TailWind css configuration on

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home/Home"
 import Navbar from './components/NavBar/NavBar';
 import './App.css';
+import './index.css';
 
 const App = () =>{  //uma notação diferente para function 
 
