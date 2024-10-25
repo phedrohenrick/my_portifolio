3154eb72fc68a22e5e78d195b013dc4155f5bc07
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-10-25 16:17:17 -0300

    Style: settling the hero page layout better

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,15 +1,16 @@
-import Avatar from "../../../../assets/images/Avatar.png"
-import {Box, Container, Grid, styled, Typography } from "@mui/material"
+import Avatar from "../../../../assets/images/Avatar.png";
+import {Box, Container, Grid, styled, Typography } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
-import AnimatedBackground from "../../../../components/AnimatedBackground/AnimatedBackground.svg";
+import AnimatedBackground from "../../../../assets/images/AnimatedBackground/AnimatedBackground.svg";
 import theme from "../../../../theme";
 import {downloadFile} from "../../../../components/Functions/DownloadFile.tsx"
 import { IconButton } from '@mui/material';
+import { motion } from "framer-motion";
 
 const Hero = () =>{  //uma notação diferente para function 
 
@@ -27,9 +28,11 @@ const Hero = () =>{  //uma notação diferente para function
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     height: "120vh",
-    width: "100vw",
+    width: "100%",
+    maxwidth: "100%",
     display: "flex",
     alignItems: "center",
+
 
     [theme.breakpoints.up('xs')]: { // <= mobile
       paddingTop: "40px",
@@ -44,28 +47,25 @@ const Hero = () =>{  //uma notação diferente para function
         width:"80%",
         borderRadius :"50%",
         border: `1px solid ${theme.palette.primary.dark}`,
-
+        
         [theme.breakpoints.up('xs')]: { // <= mobile
-          paddingTop: "0px"
-    
+          paddingTop: "0px",
+          marginLeft: "40px",
+          marginTop: "40px"
       },
       [theme.breakpoints.up('md')]: { // >=mobile
           paddingTop: "0px",
       }
   }))
   
-  const StyledImgSocials = styled("img")(() => ({
-    width:"60%",
-    margin: '-100px',
-}))
+ 
     return (
       <>
-  
       <StyledHero>
           <Container >
-              <Grid container spacing={6} >
+              <Grid container spacing={6}  >
 
-                  <Grid item xs={12} md = {5}>
+                  <Grid item xs={12} md = {5} alignItems={"center"}>
                     <Box position={"relative"}>
                     <StyledImg src= {Avatar}/>
                     </Box>
@@ -83,41 +83,45 @@ const Hero = () =>{  //uma notação diferente para function
                               <StyledButton onClick={()=> {handleDownload()}}>
                                 <DownloadIcon/>
                                 <Typography>
-                                    Download CV 
+                                    Download do Currículo 
                                 </Typography>
                               </StyledButton>
+                           
                           </Grid>
                       <Grid item  xs = {12} md={6}  display= "flex" justifyContent="center" >
                         
                       <StyledButton onClick={() => {HandleClickConatact("#footer")}} >
                           <EmailIcon />
                           <Typography>
-                           Contact me 
+                           Me contate 
                          </Typography>
                       </StyledButton>
                       </Grid>
                     </Grid>
                     <Grid container display="flex" justifyContent="center"  pt={3} marginBottom={3} >
                       <Grid item xs = {4} md={2}  display= "flex" justifyContent="center" >
-                        <IconButton href={"https://github.com/phedrohenrick"} target="_blank" sx={{
-                                color: '#dfe8e8', // Cor padrão
-                                fontSize: 50,
-                                opacity: 0.7,
-                                '@media (max-width: 880px)': { // Quando a tela for menor que 600px (breakpoint "sm")
-                                  color: '#254346', // Cor preta no mobile
+                        <motion.div whileHover={{ scale: 1.2, opacity: 1 }} whileTap={{ scale: 0.9 }}>
+                          <IconButton href={"https://github.com/phedrohenrick"} target="_blank" sx={{
+                                  color: '#dfe8e8', // Cor padrão
+                                  fontSize: 50,
                                   opacity: 0.7,
-                                },
-                              }}>
-                            <GitHubIcon sx={{ fontSize: 50 }} />
-                        </IconButton>
+                                  '@media (max-width: 899px)': { // Quando a tela for menor que 600px (breakpoint "sm")
+                                    color: '#254346', // Cor preta no mobile
+                                    opacity: 0.7,
+                                  },
+                                }}>
+                              <GitHubIcon sx={{ fontSize: 50 }} />
+                          </IconButton>
+                          </motion.div>
                       </Grid>
                       <Grid item xs = {4} md={2}  display= "flex" justifyContent="center" >
+                      <motion.div whileHover={{ scale: 1.2, opacity: 1 }} whileTap={{ scale: 0.9 }}>
                         <IconButton href={"https://www.linkedin.com/in/phedro-henrick-da-silva-le%C3%A3o-b13b96234/"} target="_blank" sx={{
                                 color: '#dfe8e8', // Cor padrão
                                 fontSize: 50,
                                 opacity: 0.7,
 
-                                '@media (max-width: 880px)': { // Quando a tela for menor que 600px (breakpoint "sm")
+                                '@media (max-width: 899px)': { // Quando a tela for menor que 600px (breakpoint "sm")
                                   color: '#254346', // Cor preta no mobile
                                   opacity: 0.7,
 
@@ -125,14 +129,16 @@ const Hero = () =>{  //uma notação diferente para function
                               }}>
                             <LinkedInIcon sx={{ fontSize: 50 }}/>
                         </IconButton>
+                        </motion.div>
                       </Grid>
                       <Grid item xs = {4} md={2}  display= "flex" justifyContent="center" >
+                      <motion.div whileHover={{ scale: 1.2, opacity: 1 }} whileTap={{ scale: 0.9 }}>
                         <IconButton href={"https://www.instagram.com/asking_dev/"} target="_blank"  sx={{
                                 color: '#dfe8e8', // Cor padrão
                                 fontSize: 50,
                                 opacity: 0.7,
 
-                                '@media (max-width: 880px)': { // Quando a tela for menor que 600px (breakpoint "sm")
+                                '@media (max-width: 899px)': { // Quando a tela for menor que 600px (breakpoint "sm")
                                   color: '#254346', // Cor preta no mobile
                                   opacity: 0.7,
 
@@ -140,6 +146,7 @@ const Hero = () =>{  //uma notação diferente para function
                               }}>
                             <InstagramIcon sx={{ fontSize: 50}}/>
                         </IconButton>
+                        </motion.div>
                       </Grid>
                       </Grid>
                   </Grid>
