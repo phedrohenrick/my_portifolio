e52da18e1be99224582e94321ffd39116d04775a
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-12-11 17:28:21 -0300

    feat: adding projects seo config

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,16 +3,26 @@ import Hero from "./sections/Hero/Hero"
 import Project from "./sections/Projects/Projects"
 import Footer from "./sections/Footer/Footer"
 import WhatPeopleThink from "./sections/WhatPeopleThink/WhatPeopleThink"
+import { Helmet } from "react-helmet"
+import { Analytics } from "@vercel/analytics/react"
 
 const Home = () =>{  //uma notação diferente para function 
 
     return (
       <>
+       <Helmet>
+        
+        <title>Phedro Henrick Portifólio </title>
+        <meta name="description" content="Portfólio de desenvolvedor especializado em React e Java. Explore meus projetos e entre em contato." />
+        <meta name="keywords" content="React, Java, Spring Boot, Portfólio, Phedro" />
+        <link rel="canonical" href="https://www.phedrohenrick-portifolio.com.br" />
+      </Helmet>
       <Hero/>
       <About/>
       <Project/>
       <WhatPeopleThink/>
       <Footer/>
+      <Analytics />
       </>
     )
   }
