b23ed17f18871d0f398ab7e730f520973189d26c
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-02-17 17:49:04 -0300

    style: changing style from navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+import { AppBar, MenuItem, styled, Toolbar, Typography } from "@mui/material"
 import { Link } from "react-scroll"; // Importa o Link do react-scroll para rolagem suave
 import { useTranslation } from 'react-i18next'
 
@@ -24,25 +24,30 @@ const NavBar = () =>{  //uma notação diferente para function
         <AppBar position="absolute">
             <StyledToolBar>
                 <MenuItem > 
-                <Link to="sobre" smooth={true} duration={300}>
-                            {t("navBarAbout")}
+                <Link to="sobre" smooth={true} duration={300} >
+
+               < Typography variant="h5"> 
+                {t("navBarAbout")}
+              </Typography>
                 </Link>
                 </MenuItem>
 
                 <MenuItem >
                 <Link to="projetos" smooth={true} duration={200}>
+                < Typography variant="h5">
                 {t("navBarProjects")}
+                </Typography>
                 </Link>
                 </MenuItem>
 
                 <MenuItem>
                 <Link to="footer" smooth={true} duration={100}>
+                < Typography variant="h5">
                 {t("navBarContact")}
+                </Typography>
                 </Link>
                 </MenuItem>
-
             </StyledToolBar>
-          
         </AppBar>      
       </>
     )
